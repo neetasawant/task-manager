9ee8d90e0d19a59d914141e12ff767a03bae05fd
neetasawant <neeta.sawant0395@gmail.com>
2025-05-08 00:18:51 +0530

    Add user auth system with JWT, bcrypt

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 // app.js
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
+app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => res.send('API is running'));
 module.exports = app;  // ✅ Must export the Express app
